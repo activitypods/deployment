1524ecca7f0342c415926e26fbd743b617d1e8a8
srosset81 <srosset81@gmail.com>
2024-12-29 11:23:42 +0100

    Revert "Set env files as env variables"
    This reverts commit e378890987f4a8c463884b4f252c0064ee4299ca.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-DOCKER_COMPOSE_PROD=docker compose
+DOCKER_COMPOSE_PROD=docker compose --env-file .env --env-file .env.local
 
 # Build and start
 
--- compact-datasets.sh
+++ compact-datasets.sh
@@ -15,6 +15,6 @@ docker compose down
 
 docker run --volume="$SCRIPT_DIR"/data/fuseki:/fuseki --entrypoint=/docker-compact-entrypoint.sh semapps/jena-fuseki-webacl
 
-docker compose up -d
+docker compose --env-file .env --env-file .env.local up -d
 
 echo "Cron job finished at" $(date)
